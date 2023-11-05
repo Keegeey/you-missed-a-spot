686383214c2fc1a5ed0dff1420158a2459a9f23a
Grant Goode <goode.grant@gmail.com>
2023-11-04 21:01:47 -0500

    Find playlists owned by current user

--- spotipy_test.py
+++ spotipy_test.py
@@ -14,16 +14,8 @@ from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-# Determine how many saved songs to print out
-if len(sys.argv) > 1:
-    NUM_SONGS = int(sys.argv[1])
-else:
-    while True:
-        try:
-            NUM_SONGS = int(input('How many songs? '))
-            break
-        except:
-            print('Please enter an integer')
+# Retrieval is quicker if done in batches, 50 seems to be the max without getting blocked
+BATCH_SIZE = 50
 
 # Grab start time to calculate total execution time
 begin_time = datetime.now()
@@ -32,49 +24,25 @@ begin_time = datetime.now()
 load_dotenv()
 
 # Use environment variables to get API authorization
-scope = 'user-library-read'
+scope = 'user-library-read,user-read-private'
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-# Retrieval is quicker if done in batches, 50 seems to be the max without getting blocked
-NUM_BATCH = 50
-MODULO = int(NUM_SONGS / NUM_BATCH)
+# Save user id
+user = spotify.current_user()
 
-# Column headers
-print('#     |  Title                                                                             |  Artist')
-print('------+------------------------------------------------------------------------------------+---------------')
+#Retrieve playlists
+playlists = spotify.current_user_playlists()
 
-# Retrieve and print songs
-for x in range(MODULO + 1):
-    # If the number of songs requested is not divisible by 50, last retrieval will be whatever is left
-    if (NUM_SONGS - (x * NUM_BATCH)) > NUM_BATCH:
-        NUM_TO_GRAB = NUM_BATCH
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        # Only check playlists which are owned by the user
+        if playlist['owner']['id'] == user['id']:
+            print(playlist['name'])
+
+    if playlists['next']:
+        playlists = spotify.next(playlists)
     else:
-        NUM_TO_GRAB = NUM_SONGS - (x * NUM_BATCH)
-    
-    # If true, we are done retrieving songs
-    if NUM_TO_GRAB == 0:
-            break
-    
-    try:
-        saved = spotify.current_user_saved_tracks(NUM_TO_GRAB, (x * NUM_BATCH))
-    except:
-        print() # \n
-        print('Something went wrong retrieving your songs.')
-        break
-
-    for idx, item in enumerate(saved['items']):
-        # Create spaces after song numbers to keep song titles aligned
-        SONG_NUM = str(idx + x * NUM_BATCH + 1)
-        for y in range(5 - len(SONG_NUM)):
-            SONG_NUM += ' '
-        track = item['track']
-
-        # Create spaces after song titles to keep artist name aligned
-        NAME_SPACES = ''
-        for z in range(80 - len(track['name'])):
-            NAME_SPACES += ' '
-
-        print(SONG_NUM, '| ', track['name'] + NAME_SPACES, ' | ', track['artists'][0]['name'])
+        playlists = None
 
 print() # \n
 
